73884df92062b63bfbaa4b9f3de483553a23ab5c
Aruuke <aruuketoktoralieva279@gmail.com>
2022-05-01 13:09:34 +0600

    added chocolates values

--- src/main/java/com/example/chocolateshop/models/Chocolate.java
+++ src/main/java/com/example/chocolateshop/models/Chocolate.java
@@ -1,4 +1,21 @@
 package com.example.chocolateshop.models;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table("chocolates")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Chocolate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
 }

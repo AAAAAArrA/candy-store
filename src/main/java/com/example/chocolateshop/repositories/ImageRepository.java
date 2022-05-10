08c417301cc365807f7eb73ff49f5d7a0cbfd987
Aruuke <aruuketoktoralieva279@gmail.com>
2022-05-10 23:17:37 +0600

    good

--- src/main/java/com/example/chocolateshop/repositories/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.chocolateshop.repositories;
-
-import com.example.chocolateshop.models.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Integer> {
-}
